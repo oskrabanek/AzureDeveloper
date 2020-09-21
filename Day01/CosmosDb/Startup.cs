5d165684deb8bb24913f2d1ce68b7f65b9733e87
Ondrej Skrabanek <OSWOndrejS@oriflame.com>
2020-09-21 15:36:00 +0200

    Update to .NET Core 3.1

--- Day01/CosmosDb/Startup.cs
+++ Day01/CosmosDb/Startup.cs
@@ -38,15 +38,16 @@ namespace todo
             services.AddTransient<IItemService, ItemService>();
 
             // Add framework services.
-            services.AddMvc();
+            services.AddMvc(options
+                =>
+            {
+                options.EnableEndpointRouting = false;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
