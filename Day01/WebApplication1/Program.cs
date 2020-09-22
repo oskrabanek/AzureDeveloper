f4819887b4c4a4ceebb1268692afe3fa66ec17c0
Ondrej Skrabanek <OSWOndrejS@oriflame.com>
2020-09-22 16:23:25 +0200

    Key Vault added

--- Day01/WebApplication1/Program.cs
+++ Day01/WebApplication1/Program.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace WebApplication1
 {
@@ -18,6 +13,11 @@ namespace WebApplication1
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var root = config.Build();
+                    config.AddAzureKeyVault($"{root["KeyVault:Url"]}", root["KeyVault:ClientId"], root["KeyVault:ClientSecret"]);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
