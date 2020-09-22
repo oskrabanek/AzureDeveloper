d0d5ea4ac9047122c14b26b831d8b018db78c29e
Ondrej Skrabanek <OSWOndrejS@oriflame.com>
2020-09-22 11:20:00 +0200

    Day 02 start

--- Day01/CosmosDb/Models/ItemService.cs
+++ Day01/CosmosDb/Models/ItemService.cs
@@ -87,7 +87,6 @@ namespace todo.Models
             }
         }
 
-
         private async Task CreateDocumentCollectionIfNotExists(string databaseName, string collectionName)
         {
             try
