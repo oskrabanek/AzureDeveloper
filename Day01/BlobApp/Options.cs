53299e0f31c0d683d09b7003a8c1a2033c35fc51
Ondřej Škrabánek <ondrej.skrabanek@oriflame.com>
2023-09-14 14:35:42 +0100

    Added BlobApp

---
+++ Day01/BlobApp/Options.cs
@@ -0,0 +1,11 @@
+﻿namespace BlobApp
+{
+    public class Options
+    {
+        public string StorageConnectionString { get; set; }
+
+        public string FullImageContainerName { get; set; }
+
+        public string ThumbnailImageContainerName { get; set; }
+    }
+}
