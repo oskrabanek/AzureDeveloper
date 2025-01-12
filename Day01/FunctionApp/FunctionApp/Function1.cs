959d42647f6f9f4b666db80777a3a27f52999f2d
Ondřej <ondrej.skrabanek@oriflame.com>
2025-01-12 21:27:42 +0100

    Day1 update

--- Day01/FunctionApp/FunctionApp/Function1.cs
+++ Day01/FunctionApp/FunctionApp/Function1.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace FunctionApp1
+namespace FunctionApp
 {
     public static class Function1
     {
--- Day01/WebApplication1/Pages/Blob.cshtml.cs
+++ Day01/WebApplication1/Pages/Blob.cshtml.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Azure.Storage.Blobs;
-using Azure.Storage.Blobs.Models;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApplication1.Pages
@@ -13,7 +8,7 @@ namespace WebApplication1.Pages
     {
         public string Containers;
 
-        public async Task OnGet()
+        public void OnGet()
         {
             string connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
 
